d93324afdaf50b9ebbfd3f2b824e78a371693d1a
Marta Lopez <mmmartttaaa@gmail.com>
2023-01-24 12:42:30 +0100

    fragment age done

---
+++ app/src/main/java/study/project/Age.java
@@ -0,0 +1,16 @@
+package study.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Age extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_age);
+
+
+    }
+}
